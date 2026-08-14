14d63a8e1520cf107ac42d4a3ead6f0a1a93add4
AhmedGamalFarouk <ahmedgamalfarouk0@gmail.com>
2026-08-15 00:37:44 +0300

    fix: resolve base path in circle-mobile and hush web runtime

--- public/demos/hush/flutter_bootstrap.js
+++ public/demos/hush/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "559237417"
+    serviceWorkerVersion: "3166998763"
   }
 });
--- public/demos/hush/flutter_service_worker.js
+++ public/demos/hush/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "9c59363edc13a5b144d47ad97b0744a8",
+"flutter_bootstrap.js": "4fcc572b30014f600c0a77944aa0c29d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
