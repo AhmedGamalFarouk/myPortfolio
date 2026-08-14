cff50500c49b68d4b6f2b89fb89087fe1cbdefdb
AhmedGamalFarouk <ahmedgamalfarouk0@gmail.com>
2026-08-15 00:15:11 +0300

    fix: resolve Hush libsodium runtime initialization on web preview

--- public/demos/hush/flutter_service_worker.js
+++ public/demos/hush/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "bdbca71634c9be85e6f83dcd4e36f8de",
+"flutter_bootstrap.js": "3e6bc59a9118731d30ca61f909f690bf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b44010a5f60e571c8626cf3b15c0ff2d",
-"/": "b44010a5f60e571c8626cf3b15c0ff2d",
-"main.dart.js": "39be52c00a930516d1ecc9ac3a09b228",
+"index.html": "02c10e186aca7b5d7e20950d831755e5",
+"/": "02c10e186aca7b5d7e20950d831755e5",
+"main.dart.js": "ed23c673a77867e0e5e60f0e62db262c",
 "manifest.json": "453369113f523511b45c78ddac7e4987",
 "version.json": "fcc64e2c89c1455f7d7b7cbc58a85ac1"};
 // The application shell files that are downloaded before a service worker can
